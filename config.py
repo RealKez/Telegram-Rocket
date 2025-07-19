1a0ecca7e9474841df0eed43e1ea31723fe63a78
D4RKV1CKY <d4rkv1cky@gmail.com>
2025-07-19 14:04:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+api_id = 1234567  # 🔒 Replace with your own
+api_hash = 'your_api_hash_here'  # 🔒 Replace with your own
+receiver = 'your_target_group_or_user'  # Example: '@mygroup'
+
+DELAY_BETWEEN_MESSAGES = 0
+DELAY_BETWEEN_LOOPS = 1
