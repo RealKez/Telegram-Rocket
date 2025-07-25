620514edf709e6c9018728324fc491d7fed8e01c
RealKez <d4rkv1cky@gmail.com>
2025-07-25 13:47:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ api_id = 1234567  # 🔒 Replace with your own
 api_hash = 'your_api_hash_here'  # 🔒 Replace with your own
 receiver = 'your_target_group_or_user'  # Example: '@mygroup'
 
-DELAY_BETWEEN_MESSAGES = 0
-DELAY_BETWEEN_LOOPS = 1
+DELAY_BETWEEN_MESSAGES = 0.5
+DELAY_BETWEEN_LOOPS = 0
